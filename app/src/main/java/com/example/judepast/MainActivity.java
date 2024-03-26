a41311814d4bdc836422afc4fc4a7bdc1c527986
Tomiloba21 <olowo15tomiloba@gmail.com>
2024-03-26 20:19:48 +0100

    added the dashboard layout

--- app/src/main/java/com/example/judepast/MainActivity.java
+++ app/src/main/java/com/example/judepast/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_intro);
+        setContentView(R.layout.dashboard);
 
 
 //        fName = findViewById(R.id.edit_fName);
